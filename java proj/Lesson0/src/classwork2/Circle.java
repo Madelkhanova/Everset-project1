391155d5d5f251911d37e8771a69f923fd26dc66
Madelkhanova <Madelxanova@mail.ru>
2023-12-14 20:35:08 +0600

    First commit

---
+++ java proj/Lesson0/src/classwork2/Circle.java
@@ -0,0 +1,34 @@
+package classwork2;
+
+import classwork1.Main;
+import classwork1.Perosn;
+
+public class Circle {
+    private static double radius =1.0;
+    private String color = "red";
+
+    public Circle(){}
+    public Circle ( double r){
+        radius=r;
+    }
+
+    public static double getRadius() {
+        return radius;
+    }
+
+    public static double getArea(){
+        return Math.PI * radius*radius;
+    }
+
+    public static double getCircumference(){
+        return 2 * Math.PI * radius*;
+    }
+
+    public String toString(){
+        return "Cicle[radius = " + radius +" ]";
+    }
+
+    public static void setRadius(double radius) {
+        Circle.radius = radius;
+    }
+}
